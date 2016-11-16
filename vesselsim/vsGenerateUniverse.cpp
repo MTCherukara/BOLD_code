dcb87eca1db405a679a23fa0454650d432fc0388
MTCherukara <matthew.cherukara@hertford.ox.ac.uk>
2016-11-16 15:18:56 +0000

    daily update

--- vesselsim/vsGenerateUniverse.cpp
+++ vesselsim/vsGenerateUniverse.cpp
@@ -6,11 +6,25 @@
     / oriented infinite cylinders (of radius R) in a spherical space, occupying total 
     volume fraction F.
 
-    Matthew Cherukara, QUBIC and FMRIB Physics Group
+    Matthew Cherukara, IBME QUBIC Group & FMRIB Physics Group
 
-    University of Oxford, 2016
-
+    Copyright (C) 2016 University of Oxford
 */
 
 #include "vsGenerateUniverse.h"
+
+#include <iostream>
+#include "newmatio.h"
+#include <stdexcept>
+
+using namespace std;
+using namespace NEWMAT;
+
+// constructor
+vsUniverse* vsUniverse::NewInstance()
+{
+    return new vsUniverse();
+}
+
+// destructor is defined in .h file
 
--- vesselsim/vsGenerateUniverse.h
+++ vesselsim/vsGenerateUniverse.h
@@ -6,14 +6,43 @@
     / oriented infinite cylinders (of radius R) in a spherical space, occupying total 
     volume fraction F.
 
-    Matthew Cherukara, QUBIC and FMRIB Physics Group
+    Matthew Cherukara, IBME QUBIC Group & FMRIB Physics Group
 
-    University of Oxford, 2016
+    Copyright (C) 2016 University of Oxford
 
 */
 
 #ifndef __VSGENERATEUNIVERSE_H
 #define __VSGENERATEUNIVERSE_H 1
+
+#include <string>
+#include <vector>
+
+#include "newmatap.h"
+
+using namespace std;
+using namespace NEWMAT;
+
+class vsUniverse {
+    public:
+        static vsUniverse* NewInstance(); // constructor
+        virtual ~FwdModel() {return; }; // destructor
+        
+        // methods for: 
+
+    protected:
+        // variables
+        int maxVessels; // 100000
+        int cutoff;     // 0, to start with
+
+        Matrix vesselOrigins;
+        Matrix vesselNormals;
+
+        double universeRadius;
+        double universeVolume;
+        double filledVolumeFraction;
+
+};
 
 
 # endif /* __VSGENERATEUNIVERSE_H */
