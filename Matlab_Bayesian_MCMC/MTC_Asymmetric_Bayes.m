b4e90a3a89ae505a2d4ff22b9f1dcd82ca661519
MTCherukara <matthew.cherukara@hertford.ox.ac.uk>
2017-04-07 17:52:35 +0100

    syncing

--- Matlab_Bayesian_MCMC/MTC_Asymmetric_Bayes.m
+++ Matlab_Bayesian_MCMC/MTC_Asymmetric_Bayes.m
@@ -34,80 +34,80 @@ S_sample = S_sample./S_sample(t0);
 
 %% Bayesian inference on a single parameter using 1D grid search
 
-% v_t = params.OEF; % true value of OEF
-% va = linspace(0,1,np); % OEF must be in interval {0,1}
-% S_model = zeros(np,ns);
-% 
-% % step through values of the parameter and calculate the model for each one
-% % of those, at all time points
-% for ii = 1:np
-%     params.OEF = va(ii);
-%     S_val = MTC_qASE_model(T_sample,params);
-%     S_model(ii,:) = S_val./S_val(t0);
-% end
-% 
-% S_samp = repmat(S_sample,np,1);
-% 
-% % compare the model against the data for each value of the parameter - this
-% % is the likelihood (which is proportional to the posterior in the case of
-% % uniform (zero) priors)
-% lik = exp(-sum((S_samp-S_model).^2,2)/sigma);
-% lik = MTC_smooth(lik,5);
-% % lik = lik/sum(lik); % normalize
-% 
-% % plot posterior (=likelihood) distribution
-% figure('WindowStyle','docked');
-% hold on; box on;
-% 
-% plot([v_t, v_t],[0, 1.1*max(lik)],'k--','LineWidth',2);
-% plot(va,lik,'-','LineWidth',2);
-% axis([min(va), max(va), 0, 1.1*max(lik)]);
-% 
-% % xlabel('Deoxygenated Blood Volume (DBV)');
-% xlabel('Oxygen Extraction Fraction (OEF)');
-% % ylabel('Posterior Distribution');
-% legend('True Value','Posterior','Location','NorthEast');
-% set(gca,'FontSize',18);
+v_t = params.zeta; % true value of OEF
+va = linspace(0,0.1,np); % OEF must be in interval {0,1}
+S_model = zeros(np,ns);
 
-%% Bayesian Inference on two parameters, using grid search
-
-tr1 = params.OEF;  % real value of OEF = 0.5
-tr2 = params.zeta; % real value of zeta = 0.03;
-
-w1 = linspace(0,1.0,np);
-w2 = linspace(0,0.1,np);
-
-pos = zeros(np,np);
-
-for i1 = 1:np
-%     disp(['Calculating iteration ',num2str(i1),' of ',num2str(np)]);
-    
-    for i2 = 1:np
-
-            params.OEF = w1(i1);
-            params.zeta = w2(i2);
-
-            S_mod = MTC_qASE_model(T_sample,params);
-            S_mod = S_mod./S_mod(t0);
-
-            pos(i1,i2) = exp(-sum((S_sample-S_mod).^2)./(sigma));
-    end
+% step through values of the parameter and calculate the model for each one
+% of those, at all time points
+for ii = 1:np
+    params.zeta = va(ii);
+    S_val = MTC_qASE_model(T_sample,params);
+    S_model(ii,:) = S_val./S_val(t0);
 end
 
-% pos = pos/sum(pos(:)); % normalize posterior
-toc;
-% plot
-figure();
-imagesc(w2,w1,pos); hold on;
-c=colorbar;
-plot([tr2,tr2],[  0, 30],'w-','LineWidth',2);
-plot([  0, 30],[tr1,tr1],'w-','LineWidth',2);
-ylabel('Oxygen Extraction Fraction (OEF)');
+S_samp = repmat(S_sample,np,1);
+
+% compare the model against the data for each value of the parameter - this
+% is the likelihood (which is proportional to the posterior in the case of
+% uniform (zero) priors)
+lik = exp(-sum((S_samp-S_model).^2,2)/sigma);
+lik = MTC_smooth(lik,7);
+% lik = lik/sum(lik); % normalize
+
+% plot posterior (=likelihood) distribution
+figure('WindowStyle','docked');
+hold on; box on;
+
+plot([v_t, v_t],[0, 1.1*max(lik)],'k--','LineWidth',2);
+plot(va,lik,'-','LineWidth',4);
+axis([min(va), max(va), 0, 1.1*max(lik)]);
+
 xlabel('Deoxygenated Blood Volume (DBV)');
-ylabel(c,'Posterior Probability Density');
-title(['SNR = ',num2str(1/sigma)]);
-axis([min(w2),max(w2),min(w1),max(w1)]);
-set(gca,'FontSize',18,'YDir','normal');
-set(c,'FontSize',20)
-set(gcf,'WindowStyle','docked');
+% xlabel('Oxygen Extraction Fraction (OEF)');
+% ylabel('Posterior Distribution');
+legend('True Value','Posterior','Location','NorthEast');
+set(gca,'FontSize',16);
+
+%% Bayesian Inference on two parameters, using grid search
+% 
+% tr1 = params.OEF;  % real value of OEF = 0.5
+% tr2 = params.zeta; % real value of zeta = 0.03;
+% 
+% w1 = linspace(0,1.0,np);
+% w2 = linspace(0,0.1,np);
+% 
+% pos = zeros(np,np);
+% 
+% for i1 = 1:np
+% %     disp(['Calculating iteration ',num2str(i1),' of ',num2str(np)]);
+%     
+%     for i2 = 1:np
+% 
+%             params.OEF = w1(i1);
+%             params.zeta = w2(i2);
+% 
+%             S_mod = MTC_qASE_model(T_sample,params);
+%             S_mod = S_mod./S_mod(t0);
+% 
+%             pos(i1,i2) = exp(-sum((S_sample-S_mod).^2)./(sigma));
+%     end
+% end
+% 
+% % pos = pos/sum(pos(:)); % normalize posterior
+% toc;
+% % plot
+% figure();
+% imagesc(w2,w1,pos); hold on;
+% c=colorbar;
+% plot([tr2,tr2],[  0, 30],'w-','LineWidth',2);
+% plot([  0, 30],[tr1,tr1],'w-','LineWidth',2);
+% ylabel('Oxygen Extraction Fraction (OEF)');
+% xlabel('Deoxygenated Blood Volume (DBV)');
+% ylabel(c,'Posterior Probability Density');
+% title(['SNR = ',num2str(1/sigma)]);
+% axis([min(w2),max(w2),min(w1),max(w1)]);
+% set(gca,'FontSize',18,'YDir','normal');
+% set(c,'FontSize',20)
+% set(gcf,'WindowStyle','docked');
 
