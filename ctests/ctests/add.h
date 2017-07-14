dfe114d989583d8f93e5d45164c2921c6d97459a
Matthew Cherukara <matthew.cherukara@hertford.ox.ac.uk>
2017-07-14 13:54:47 +0100

    testing

---
+++ ctests/ctests/add.h
@@ -0,0 +1,15 @@
+//
+//  add.h
+//  ctests
+//
+//  Created by Matthew Cherukara on 14/07/2017.
+//  Copyright © 2017 Matthew Cherukara. All rights reserved.
+//
+
+#ifndef ADD_H
+#define ADD_H
+
+int add(int x, int y);
+
+
+#endif /* ADD_H */
---
+++ ctests/ctests/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  ctests
+//
+//  Created by Matthew Cherukara on 14/07/2017.
+//  Copyright © 2017 Matthew Cherukara. All rights reserved.
+//
+
+#include <iostream>
+#include "add.h"
+
+int main()
+{
+    std::cout << "3 plus 4 equals " << add(3,4) << std::endl;
+    return 0;
+}
+
