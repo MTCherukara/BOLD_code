ecdb44e28f22cd08857309f60a847fc2c79eadc5
Matthew Cherukara <matthew.cherukara@hertford.ox.ac.uk>
2017-12-08 11:30:42 +0000

    edit header python

--- C_FABBER/zaverage.py
+++ C_FABBER/zaverage.py
@@ -18,12 +18,10 @@ def make_temp_dir():
 def remove_temp_dir(dirname):
     """Removes the directory that was set up earlier"""
     subprocess.check_call(["rm", "-r", dirname])
-    return
 
 def split_data(dirname, filename):
     """Split the ASE image into separate slices"""
     subprocess.check_call(["fslslice", filename, ''.join([dirname, "/data"])])
-    return
 
 def count_slices(filename):
     """Return the number of slices in the ASE image"""
@@ -34,6 +32,55 @@ def count_slices(filename):
 def name_slice(dirname, slicenum):
     """Return a string containing the name of a single-slice nifti"""
     return ''.join([dirname, "/data_slice_", str(slicenum).zfill(4)])
+
+def read_header(niiname, txtname="tmp_hdr.txt"):
+    """Read a nifti header file NIINAME and write its contents to a text file TXTNAME"""
+    # Call FSLHD
+    hdr_pipe = subprocess.Popen(["fslhd", "-x", niiname], stdout=subprocess.PIPE)
+    hdr_str = hdr_pipe.stdout.read().decode()
+    
+    # Split the string
+    hdr_arr = filter(None, hdr_str.split("\n"))
+
+    # Write it out element by element
+    with open(txtname, "a") as txt_file:
+        for hdr_entry in hdr_arr:
+            print(hdr_entry, file=txt_file)
+
+def edit_header(niiname, field, value):
+    """Edit nifti header NIINAME such that FIELD has a new VALUE""" 
+    # First read the header
+    read_header(niiname, "killme.hdr.txt")
+
+    # Then read the contents of the header into an array
+    with open("killme.hdr.txt","r") as txt_hdr:
+        hdr_arr = txt_hdr.readlines()
+    
+    # Open a new text file to write to line by line
+    with open("killme.newhdr.txt","a") as new_hdr:
+
+        # Search through elements
+        for element in hdr_arr:
+
+            # Find the element we want to change
+            if "".join([field," ="]) in element:
+
+                # Change it
+                elem = element.split(" = ")
+                elem[1] = "'{}'\n".format(value)
+
+                # Write it out
+                new_hdr.write("".join([elem[0],' = ',elem[1]]))
+            else:
+                new_hdr.write(element)
+
+
+    # Write out the edited header
+    p = subprocess.Popen(["fslcreatehd", "killme.newhdr.txt", niiname])
+    p.communicate()
+
+    # Delete the temporary header text files
+    subprocess.check_call(["rm","killme.hdr.txt","killme.newhdr.txt"])
 
 def average_slices(dirname, numslices):
     """Averages sets of 4 slices, up to numslices slices (e.g. 10)"""
@@ -57,13 +104,14 @@ def average_slices(dirname, numslices):
         subprocess.check_call(["fslmaths", slice_1, "-add", slice_2, "-add", \
                                slice_3, "-add", slice_4, "-div", str(4), slice_av])
 
-        # Sort out the Z-spacing in the header
-        hdr_pipe = subprocess.Popen(["fslhd", "-x", slice_av], stdout=subprocess.PIPE)
-        open("tmp_hdr.txt", "a").write(str(hdr_pipe.stdout.read()))
-        
+    # Merge new slices together
+
+    # Correct the slice spacing dz
+
+    # Delete everything that was left over
         
 
-    return
+    return 1
 
 # Main Function
 if __name__ == "__main__":
@@ -77,6 +125,10 @@ if __name__ == "__main__":
     FILE_IN_NAME = sys.argv[1]
     FILE_OUT_NAME = sys.argv[2]
 
+    # Testing of read_header function
+    edit_header(FILE_IN_NAME,"dz","7.5")
+
+    """
     # Set up temporary directory
     TEMPDIR = make_temp_dir()
 
@@ -91,3 +143,4 @@ if __name__ == "__main__":
 
     # Delete the directory
     #remove_temp_dir(TEMPDIR)
+    """
