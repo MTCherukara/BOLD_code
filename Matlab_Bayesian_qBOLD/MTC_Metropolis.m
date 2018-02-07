b4104e513671aeef69c8d2aed45fb0a73d9eb905
Matthew Cherukara <matthew.cherukara@hertford.ox.ac.uk>
2018-02-07 13:51:33 +0000

    plotting properly ;

--- Matlab_Bayesian_qBOLD/MTC_Metropolis.m
+++ Matlab_Bayesian_qBOLD/MTC_Metropolis.m
@@ -37,6 +37,11 @@
 clear; 
 close all;
 
+% Options
+plot_trace = 0;
+plot_results = 1;
+save_results = 0;
+
 % Load Data
 load('ASE_Data/Data_MultiTE_180208_SNR_200.mat');
 params_true = params;
@@ -44,7 +49,7 @@ params_true = params;
 
 % Parameter Values
 p_names = { 'OEF'; 'R2p'; 'zeta'; 'R2t' ; 'geom' };
-p_infer = [   1  ,   0  ,   0   ,   1   ,  0     ];
+p_infer = [   1  ,   0  ,   1   ,   1   ,  0     ];
 p_inits = [  0.5 ,  4.0 ,  0.026,  10.0 ,  0.3   ];
 p_range = [  0.2 ,  3.0 ,  0.02 ,   5.0 ,  0.1    ;...
              0.6 ,  5.5 ,  0.04 ,  15.0 ,  0.5   ];
@@ -68,10 +73,10 @@ end
 np = sum(p_infer);
 
 %% Metropolis Parameters
-j_brn  = 10000;      % number of jumps in the 'burn-in' phase
-j_run  = 10;      % number of jumps in the real thing
-j_updt = 10;       % rate of updating the scaling parameter
-j_rng  = 500;       % range of samples to look over when updating scaling param
+j_run  = 1e5;       % number of jumps in the real thing
+j_updt = 10;        % rate of updating the scaling parameter
+j_rng  = 200;       % range of samples to look over when updating scaling param
+j_brn  = (200*j_updt) + j_rng;      % number of jumps in the 'burn-in' phase
 
 % counters
 c_rt  = 0;      % rate counter
@@ -183,173 +188,95 @@ toc;
 
 
 %% Display Acceptance Rate Trend
-% figure('WindowStyle','Docked');
-figure;
-hold on; box on;
-plot(accept_rate(1:end-1),'k-','LineWidth',1);
-xlabel('Iterations');
-ylabel('Sample Acceptance Rate');
-title(['Trace Plot (update range = ',num2str(j_rng),')']);
-% set(gca,'FontSize',14);
 
-
-% %% Scatter Plot
-% % only when there are fewer than 20k points, otherwise the whole thing is just a
-% % massive splodge
-% 
-% if ( np == 2 && size(sample_results,2) < 20000)
-%     figure('WindowStyle','Docked');
-%     hold on; box on;
-% 
-%     % plot true values
-%     % plot([params_true.OEF,params_true.OEF],[p_rng(1,2),p_rng(2,2)]  ,'r-','LineWidth',2);
-%     plot([p_rng(1,1),p_rng(2,1)],[params_true.zeta,params_true.zeta],'r-','LineWidth',2);
-%     plot([params_true.R2p,params_true.R2p],[p_rng(1,2),p_rng(2,2)]  ,'r-','LineWidth',2);
-% 
-%     % plot results
-%     scatter(sample_results(1,:),sample_results(2,:),'k.');
-% 
-%     xlabel(p_name{1});
-%     ylabel(p_name{2});
-%     set(gca,'FontSize',14);
-% end
-
-%% Contour Plot
-% % in two dimensions
-% 
-% if np == 2
-%     
-%     ctres = 25; % contour resolution
-% 
-%     figure('WindowStyle','Docked');
-%     hold on; box on;
-% 
-%     if infer_R2p
-%         plot([p_rng(1,2),p_rng(2,2)], [params_true.R2p,params_true.R2p],'k-','LineWidth',2);
-%     else
-%         plot([p_rng(1,2),p_rng(2,2)], [params_true.OEF,params_true.OEF],'k-','LineWidth',2);
-%     end
-%     plot([params_true.zeta,params_true.zeta], [p_rng(1,1),p_rng(2,1)],'k-','LineWidth',2);
-% 
-%     [n,c] = hist3(sample_results',[ctres,ctres]);
-%     contour(c{2},c{1},n);
-% 
-%     xlabel(p_name{2});
-%     ylabel(p_name{1});
-%     set(gca,'FontSize',18);
-%     
-% end % if np == 2
-
-
-%% 3D Analysis
-
-% if np > 2
-%     
-%     ctres = 25; % contour resolution
-% 
-%     % choose which parameter 'plane' to look at
-%     an_param = 3;       % 3 = R2
-%     an_value = 11.0;
-%     an_range = p_rng(2,an_param) - p_rng(1,an_param);
-% 
-%     % how big a slice of the 3D volume?
-%     slabwidth = 0.1;    % 10%
-% 
-%     % define the boundaries of the slab
-%     slabup = an_value + (slabwidth*an_range/2);
-%     slabdn = an_value - (slabwidth*an_range/2);
-% 
-%     % identify which points are within the slab
-%     slab_vals = sample_results(an_param,:);
-%     slab_log = logical((slab_vals > slabdn) .* (slab_vals < slabup));
-% 
-%     % extract points from within the slab
-%     slab_data = sample_results(1:2,slab_log);
-% 
-%     % plot    
-%     figure('WindowStyle','Docked');
-%     hold on; box on;
-%     
-%     if infer_R2p
-%         plot([params_true.R2p,params_true.R2p],[p_rng(1,2),p_rng(2,2)]  ,'k-','LineWidth',2);
-%     else
-%         plot([params_true.OEF,params_true.OEF],[p_rng(1,2),p_rng(2,2)]  ,'k-','LineWidth',2);
-%     end
-%     plot([p_rng(1,1),p_rng(2,1)],[params_true.zeta,params_true.zeta],'k-','LineWidth',2);
-%     
-%     [n,c] = hist3(slab_data',[ctres,ctres]);
-%     contour(c{1},c{2},n);
-%     
-%     xlabel(p_name{1});
-%     ylabel(p_name{2});
-%     set(gca,'FontSize',14);
-% 
-%     
-% end % if np > 2
-
-
-%% High Dimensional Result Presentation
-
-% resolutions
-hres = 25;
-cres = 25;
-
-% individual parameter histograms
-for pp = 1:np
+if plot_trace
     
-    figure('WindowStyle','Docked');
-    hold on; box on;
-    histogram(sample_results(pp,:),hres);
-    xlabel(p_name{pp});
-    xlim(p_rng(:,pp));
-    set(gca,'FontSize',18);
-    
-end
-
-p_pairs = combnk(1:np,2);
-
-% pair-wise scatter or contour plots
-for pp = 1:size(p_pairs,1)
-    
-    % parameters
-    prm1 = p_pairs(pp,1);
-    prm2 = p_pairs(pp,2);
-    
-    % true values
-    true1 = eval(['params_true.',p_name{prm1}]);
-    true2 = eval(['params_true.',p_name{prm2}]);
-    
-    % make figure    
     figure; hold on; box on;
+    plot(accept_rate(1:end-1),'k-','LineWidth',1);
+    ylim([0,1]);
     
-    % For a small number of samples, do a scatter plot
-    if size(sample_results,2) < 20000
-        
-        % plot scatter
-        scatter(sample_results(prm1,:),sample_results(prm2,:),'k.');
-        
-        % in this case, plot the true values on top, in red
-        plot([p_rng(1,prm2),p_rng(2,prm2)],[true1,true1],'r-');
-        plot([true2,true2],[p_rng(1,prm1),p_rng(2,prm1)],'r-');
-
-        
-    % Otherwise, make a contour plot
-    else
-        
-        % collect contour points
-        [n,c] = hist3(sample_results([prm1,prm2],:)',[cres,cres]);
-        
-        % this time, put the true values underneath the countour lines, in black
-        plot([p_rng(1,prm2),p_rng(2,prm2)],[true1,true1],'k-');
-        plot([true2,true2],[p_rng(1,prm1),p_rng(2,prm1)],'k-');
-
-        % plot
-        contour(c{2},c{1},n);
-        
-    end
+    xlabel('Iterations');
+    ylabel('Sample Acceptance Rate');
+    title(['Trace Plot (update range = ',num2str(j_rng),')']);
     
-    % labels
-    xlabel(p_name{prm2});
-    ylabel(p_name{prm1});   
-        
 end
+
+
+%% Results
+
+if plot_results
+
+    % resolutions
+    hres = 25;
+    cres = 25;
+
+    % individual parameter histograms
+    for pp = 1:np
+
+        figure; hold on; box on;
+        histogram(sample_results(pp,:),hres);
+        xlabel(p_name{pp});
+        xlim(p_rng(:,pp));
+        
+        if save_results
+            ftitle = strcat('temp_plots/MH_',date,'_Hist_',p_name{pp});
+            export_fig([ftitle,'.eps']);
+        end
+
+    end
+
+    p_pairs = combnk(1:np,2);
+
+    % pair-wise scatter or contour plots
+    for pp = 1:size(p_pairs,1)
+
+        % parameters
+        prm1 = p_pairs(pp,1);
+        prm2 = p_pairs(pp,2);
+
+        % true values
+        true1 = eval(['params_true.',p_name{prm1}]);
+        true2 = eval(['params_true.',p_name{prm2}]);
+
+        % make figure    
+        figure; hold on; box on;
+
+        % For a small number of samples, do a scatter plot
+        if size(sample_results,2) < 20000
+
+            % plot scatter
+            scatter(sample_results(prm2,:),sample_results(prm1,:),'k.');
+
+            % in this case, plot the true values on top, in red
+            plot([p_rng(1,prm2),p_rng(2,prm2)],[true1,true1],'r-');
+            plot([true2,true2],[p_rng(1,prm1),p_rng(2,prm1)],'r-');
+
+
+        % Otherwise, make a contour plot
+        else
+
+            % collect contour points
+            [n,c] = hist3(sample_results([prm1,prm2],:)',[cres,cres]);
+
+            % this time, put the true values underneath the countour lines, in black
+            plot([p_rng(1,prm2),p_rng(2,prm2)],[true1,true1],'k-');
+            plot([true2,true2],[p_rng(1,prm1),p_rng(2,prm1)],'k-');
+
+            % plot
+            contour(c{2},c{1},n);
+
+        end % if size(sample_results,2) < 20000
+
+        % labels
+        xlabel(p_name{prm2});
+        ylabel(p_name{prm1});   
+        
+        if save_results
+            ftitle = strcat('temp_plots/MH_',date,'_2D_',...
+                            p_name{prm2},'_',pname{prm1});
+            export_fig([ftitle,'.eps']);
+        end
+
+    end % for pp = 1:size(p_pairs,1)
+    
+end % if plot_results
