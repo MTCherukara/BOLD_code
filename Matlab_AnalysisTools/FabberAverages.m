828d9ceecfb998de05a2f130ef0059a44381aa7a
Matthew Cherukara <matthew.cherukara@hertford.ox.ac.uk>
2018-03-07 13:31:05 +0000

    redone averages

--- Matlab_AnalysisTools/FabberAverages.m
+++ Matlab_AnalysisTools/FabberAverages.m
@@ -36,12 +36,12 @@ maskslice = LoadSlice(['/Users/mattcher/Documents/DPhil/Data/validation_sqbold/v
 %% Load data
 DBVslice = LoadSlice([fabdir,'mean_DBV.nii.gz'],slicenum);
 R2pslice = LoadSlice([fabdir,'mean_R2p.nii.gz'],slicenum);
-% OEFslice  = LoadSlice([fabdir,'mean_OEF.nii.gz'],slicenum);
+OEFslice  = LoadSlice([fabdir,'mean_OEF.nii.gz'],slicenum);
 DBV_std  = LoadSlice([fabdir,'std_DBV.nii.gz'],slicenum);
 R2p_std  = LoadSlice([fabdir,'std_R2p.nii.gz'],slicenum);
 
 % Mask
-% OEFslice  = OEFslice.*maskslice;
+OEFslice  = OEFslice.*maskslice;
 DBVslice = DBVslice.*maskslice;
 R2pslice = R2pslice.*maskslice;
 DBV_std = DBV_std.*maskslice;
@@ -50,12 +50,14 @@ R2p_std = R2p_std.*maskslice;
 % remove zeros, etc
 DBVslice = abs(DBVslice(:));
 DBVslice(DBVslice == 0) = [];
+DBVslice(DBVslice > 1) = 1;
 R2pslice = abs(R2pslice(:));
 R2pslice(R2pslice == 0) = [];
 
-% OEFslice = abs(OEFslice(:));
-% OEFslice(OEFslice == 0) = [];
-% OEFslice(isnan(OEFslice)) = [];
+OEFslice = abs(OEFslice(:));
+OEFslice(OEFslice == 0) = [];
+OEFslice(OEFslice > 1) = 1;
+OEFslice(isnan(OEFslice)) = [];
 
 DBV_std = (DBV_std(:));
 DBV_std(DBV_std == 0) = [];
@@ -83,24 +85,28 @@ disp(['  Results for ',fdname.name]);
 
 
 disp('   ');
-% disp(['Mean R2''  : ',num2str(mean(R2pslice))]);
-disp(['Median R2'': ',num2str(median(R2pslice),4)]);
-R2Q = quantile(R2pslice,[0.75,0.25]);
-disp(['   R2'' Mean Error  : ',num2str(nanmean(R2p_std),2)]);
-disp(['   R2'' IQR: ',num2str((R2Q(1)-R2Q(2))./2,3)]);
+disp(['Mean R2''  : ',num2str(mean(R2pslice),4)]);
+% disp(['   Std R2'': ',num2str(std(R2pslice),3)]);
+% disp(['Median R2'': ',num2str(median(R2pslice),4)]);
+% R2Q = quantile(R2pslice,[0.75,0.25]);
+disp(['   R2'' Mean Error  : ',num2str(nanmean(R2p_std),3)]);
+% disp(['   R2'' IQR: ',num2str((R2Q(1)-R2Q(2))./2,3)]);
 % disp(['R2'' Median Error: ',num2str(median(R2p_std))]);
 
 disp('   ');
-% disp(['Mean DBV  : ',num2str(100*mean(DBVslice))]);
-disp(['Median DBV: ',num2str(100.*median(DBVslice),4)]);
-DBQ = quantile(DBVslice,[0.75,0.25]);
+disp(['Mean DBV  : ',num2str(100*mean(DBVslice),4)]);
+% disp(['   Std DBV: ',num2str(100*std(DBVslice),3)]);
+% disp(['Median DBV: ',num2str(100.*median(DBVslice),4)]);
+% DBQ = quantile(DBVslice,[0.75,0.25]);
 disp(['   DBV Mean Error  : ',num2str(100*nanmean(DBV_std),3)]);
-disp(['   DBV IQR: ',num2str(50.*(DBQ(1)-DBQ(2)),3)]);
+% disp(['   DBV IQR: ',num2str(50.*(DBQ(1)-DBQ(2)),3)]);
 % disp(['DBV Median Error: ',num2str(100*median(DBV_std))]);
 
 disp('   ');
 % disp(['Mean OEF  : ',num2str(100*mean(R2pslice)/(301.74*mean(DBVslice)))]);
-disp(['Median OEF: ',num2str(100*median(R2pslice)/(301.74*median(DBVslice)),4)]);
+disp(['Mean OEF  : ',num2str(100*mean(OEFslice),4)]);
+% disp(['   Std OEF: ',num2str(100*std(OEFslice),3)]);
+% disp(['Median OEF: ',num2str(100*median(R2pslice)/(301.74*median(DBVslice)),4)]);
 % disp(['Median OEF: ',num2str(100.*median(OEFslice))]);
 % OEQ = quantile(OEFslice,[0.75,0.25]);
 % disp(['   OEF IQR: ',num2str(50.*(OEQ(1)-OEQ(2)))]);
